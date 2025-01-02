78d29542e77f5d3070bc082e0d03dc876c52e46d
mohsin khan <mohsin21.kh@gmail.com>
2025-01-02 15:00:50 +0530

    program of array fibonacci

---
+++ fib_array.js
@@ -0,0 +1,13 @@
+let fib =[];
+let first = 1;
+let second = 1;
+let n = 7;
+fib.push(first,second);
+
+for(let i = 1; i<=n; i++){
+    let sum = first +second;
+    fib.push(sum);
+    second = first;
+    first = sum;
+}
+console.log(fib);
